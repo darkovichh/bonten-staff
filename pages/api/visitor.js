1134217302a82ec047bd9881495fc1d6b9b2d99e
darkovichh <semsixfw@gmial.com>
2025-08-16 04:22:47 +0400

    global.css v15.3

--- pages/api/visitor.js
+++ pages/api/visitor.js
@@ -5,28 +5,27 @@ export default async function handler(req, res) {
     const client = await clientPromise;
     const db = client.db(process.env.MONGODB_DB);
 
-    // Kullanıcının gerçek IP'sini al
+    // Kullanıcının gerçek IP'si
     let ip =
       req.headers["x-forwarded-for"]?.split(",")[0] || 
       req.socket.remoteAddress || 
       "Bilinmiyor";
 
-    // Localhost testi için dummy IP (Google DNS)
+    // Localhost testi için dummy IP
     if (ip === "::1" || ip === "127.0.0.1") ip = "8.8.8.8";
 
     let country = "Bilinmiyor";
     let city = "Bilinmiyor";
 
     try {
-      const geoRes = await fetch(`https://ipapi.co/${ip}/json/`);
+      const geoRes = await fetch(`https://ipinfo.io/${ip}/json?token=YOUR_TOKEN`);
       const geoData = await geoRes.json();
-      country = geoData.country_name || "Bilinmiyor";
+      country = geoData.country || "Bilinmiyor";
       city = geoData.city || "Bilinmiyor";
     } catch (e) {
       console.error("GeoIP hatası:", e);
     }
 
-    // Ziyaretçi kaydı
     await db.collection("visitors").insertOne({
       ip,
       country,
